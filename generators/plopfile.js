f2c4284e289a3ebb5a3064fcb7a212b34fd77668
renatocieb <renato@cieb.net.br>
2023-05-26 15:02:54 -0300

    feat: add plopfiles

---
+++ generators/plopfile.js
@@ -0,0 +1,34 @@
+module.exports = (plop) => {
+  plop.setGenerator("component", {
+    description: "application component logic",
+    prompts: [
+      {
+        type: "input",
+        name: "path",
+        message: "path",
+      },
+      {
+        type: "input",
+        name: "name",
+        message: "component",
+      },
+    ],
+    actions: [
+      {
+        type: "add",
+        path: "../src/{{path}}/{{pascalCase name}}/index.tsx",
+        templateFile: "../templates/index.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/{{path}}/{{pascalCase name}}/test.tsx",
+        templateFile: "../templates/test.tsx.hbs",
+      },
+      {
+        type: "add",
+        path: "../src/{{path}}/{{pascalCase name}}/index.stories.tsx",
+        templateFile: "../templates/index.stories.tsx.hbs",
+      },
+    ],
+  });
+};
