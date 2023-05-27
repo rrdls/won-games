4b8170e8a95b155dde3a68cd4946cdb84dc0e596
renatocieb <renato@cieb.net.br>
2023-05-27 12:02:08 -0300

    feat: remove unused files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,9 @@
-import Main from "@/shared-components/Main";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-200">
       <h1>Vai tomar no cu</h1>
-      <Main />
     </main>
   );
 }
