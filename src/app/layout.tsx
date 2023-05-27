1219767914dd52c6e42868c64035180922b1d0d9
renatocieb <renato@cieb.net.br>
2023-05-26 21:14:53 -0300

    feat: add fonts variables and colors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,11 @@
 import "./globals.css";
-import { Inter } from "next/font/google";
+import { Poppins } from "next/font/google";
 
-const inter = Inter({ subsets: ["latin"] });
+const roboto = Poppins({
+  subsets: ["latin"],
+  weight: ["300", "400", "600"],
+  variable: "--font-poppins",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -15,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${roboto.variable} font-poppins`}>{children}</body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,21 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        poppins: "var(--font-poppins)",
+      },
+      colors: {
+        primary: "#F231A5",
+        secondary: "#3CD3C1",
+        mainBg: "#06092B",
+        white: "#FAFAFA",
+        black: "#030517",
+        lightGray: "#EAEAEA",
+        gray: "#8F8F8F",
+        darkGray: "#2E2F42",
+      },
+    },
   },
   plugins: [],
 };
