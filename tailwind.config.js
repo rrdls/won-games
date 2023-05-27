bfcf13d88fd2065d6202321a41989951e2bf3ca1
renatocieb <renato@cieb.net.br>
2023-05-27 17:37:50 -0300

    feat: change breakpoints to use max-width

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,13 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      "2xl": { max: "1535px" },
+      xl: { max: "1279px" },
+      lg: { max: "1023px" },
+      md: { max: "767px" },
+      sm: { max: "639px" },
+    },
     extend: {
       fontFamily: {
         poppins: "var(--font-poppins)",
