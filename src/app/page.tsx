e15cbc7685feb7d0cf3f834e5cea3e0366748feb
renatocieb <renato@cieb.net.br>
2023-05-27 21:36:10 -0300

    feat: chante the page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,7 @@
-import Image from "next/image";
+import Heading from "@/components/Heading";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-blue-200">
-      <h1>Vai tomar no cu</h1>
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   );
 }
